b078b860757b1d9ffc31ad22429e558ab154d8cc
Oliver Laslett <O.Laslett@soton.ac.uk>
2015-01-28 16:52:27 +0000

    Cleaned up shell script

--- test_notebooks_in-folder.sh
+++
@@ -1,7 +0,0 @@
-for FILE in $(find $1 -not -path '*/\.*' -path '*.ipynb')
-do
-	echo "\n"
-	echo "=========== TESTING $(echo $FILE | grep "\w*\.ipynb") ==============\n"
-        echo "===================================================================\n"	
-	python parsenb.py $FILE
-done
