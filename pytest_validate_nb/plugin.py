42f8439523446b7fd256494223f7b702b3c9d6e9
Maximilian Albert <maximilian.albert@soton.ac.uk>
2015-04-08 15:47:58 +0100

    Remove long docstring which duplicates info from the IPython message spec available online.

--- pytest_validate_nb/plugin.py
+++ pytest_validate_nb/plugin.py
@@ -93,24 +93,6 @@ class RunningKernel(object):
         self.km, self.kc = start_new_kernel(extra_arguments=['--matplotlib=inline'],
                                             stderr=open(os.devnull, 'w'))
         # We need iopub to read every line in the cells
-        """
-        http://ipython.org/ipython-doc/stable/development/messaging.html
-
-        IOPub: this socket is the 'broadcast channel' where the kernel
-        publishes all side effects (stdout, stderr, etc.) as well as the
-        requests coming from any client over the shell socket and its
-        own requests on the stdin socket. There are a number of actions
-        in Python which generate side effects: print() writes to sys.stdout,
-        errors generate tracebacks, etc. Additionally, in a multi-client
-        scenario, we want all frontends to be able to know what each other
-        has sent to the kernel (this can be useful in collaborative scenarios,
-        for example). This socket allows both side effects and the information
-        about communications taking place with one client over the shell
-        channel to be made available to all clients in a uniform manner.
-
-        Check: stderr and stdout in the NbCellError function at the end
-        (if we get an error, check the msg_type and make the test to fail)
-        """
         self.iopub = self.kc.iopub_channel
 
     # These options are in case we wanted to restart the nb every time
