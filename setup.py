9369a4261c9367909624803c7dbccd260f467c2d
Oliver Laslett <o.laslett@soton.ac.uk>
2016-03-18 10:00:11 +0000

    rename, add version and authors

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup
 
 setup(
-    name="pytest_validate_nb",
-    packages = ['pytest_validate_nb'],
+    name="nbval",
+    version="0.2",
+    author="Laslett, Cortes, Kluyver, Fangohr",
+    packages = ['nbval'],
 
     # the following makes a plugin available to pytest
     entry_points = {
         'pytest11': [
-            'name_of_plugin = pytest_validate_nb.plugin',
+            'name_of_plugin = nbval.plugin',
         ]
     },
 )
