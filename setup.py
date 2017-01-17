57fa315dbc607a2f4a005a4ee5e84749a1e96155
Thomas Kluyver <thomas@kluyver.me.uk>
2017-01-17 10:19:24 +0000

    Version number -> 0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="nbval",
-    version="0.3.6",
+    version="0.4.0",
     author="Laslett, Cortes, Kluyver, Pepper, Fangohr",
     description='A py.test plugin to validate Jupyter notebooks',
     packages = ['nbval'],
