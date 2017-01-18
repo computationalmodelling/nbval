c960e7d70b689b7638b8390b266932a5a4456ac0
Hans Fangohr <fangohr@soton.ac.uk>
2017-01-18 12:12:12 +0000

    heavy code duplication (triangulation), but works.
    Should create pytest test items automatically next
    to eleminate duplication.

--- tests/test_unit_tests_in_notebooks.py
+++ tests/test_unit_tests_in_notebooks.py
@@ -24,3 +24,37 @@ def test_latex_fail_randomoutput():
     # test-latex-fail-randomoutput.ipynb
     # test-latex-pass-correctouput.ipynb
     # test-latex-pass-failsbutignoreoutput.ipynb
+
+
+def test_latex_pass_correctoutput():
+
+    testname = "test-latex-pass-correctouput"
+    command = command_template + testname + ".ipynb"
+
+    correct_outcome = testname.split('-')[2]
+    assert correct_outcome in ['pass', 'fail']
+
+    exitcode = subprocess.call(command, shell=True)
+    if correct_outcome is 'pass':
+        assert exitcode is 0
+    elif correct_outcome is 'fail':
+        assert exitcode is not 0
+
+
+def test_latex_pass_failsbutignoreoutput():
+
+    testname = "test-latex-pass-failsbutignoreoutput"
+    command = command_template + testname + ".ipynb"
+
+    correct_outcome = testname.split('-')[2]
+    assert correct_outcome in ['pass', 'fail']
+
+    exitcode = subprocess.call(command, shell=True)
+    if correct_outcome is 'pass':
+        assert exitcode is 0
+    elif correct_outcome is 'fail':
+        assert exitcode is not 0
+
+
+    # test-latex-pass-correctouput.ipynb
+    # test-latex-pass-failsbutignoreoutput.ipynb
