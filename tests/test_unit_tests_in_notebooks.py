2950ce606ccd0e894aecceb8bdcc9fbe1b93b22b
Hans Fangohr <fangohr@soton.ac.uk>
2017-01-18 12:09:20 +0000

    first attempt of testing failures more systematically

---
+++ tests/test_unit_tests_in_notebooks.py
@@ -0,0 +1,26 @@
+import subprocess
+
+command_template = "py.test --nbval -v "
+
+
+def runtest(command):
+    pass
+
+
+def test_latex_fail_randomoutput():
+
+    testname = "test-latex-fail-randomoutput"
+    command = command_template + testname + ".ipynb"
+
+    correct_outcome = testname.split('-')[2]
+    assert correct_outcome in ['pass', 'fail']
+
+    exitcode = subprocess.call(command, shell=True)
+    if correct_outcome is 'pass':
+        assert exitcode is 0
+    elif correct_outcome is 'fail':
+        assert exitcode is not 0
+
+    # test-latex-fail-randomoutput.ipynb
+    # test-latex-pass-correctouput.ipynb
+    # test-latex-pass-failsbutignoreoutput.ipynb
