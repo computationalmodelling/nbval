806dd84fa342af0e640f177f15c8adad620200d5
Thomas Kluyver <thomas@kluyver.me.uk>
2018-01-15 12:30:53 +0000

    Fix DOIT_CONFIG, verbose default for all tasks

--- dodo.py
+++ dodo.py
@@ -1,20 +1,21 @@
 PYTHON = "python3"
 
-DOUT_CONFIG = {'default_tasks': ['test']}
+DOIT_CONFIG = {
+    'default_tasks': ['test'],
+    'verbosity': 2,
+}
 
 def task_test():
     return {
         'actions': [
             ["py.test", "-v", "tests/", "--nbval", "--current-env", "--sanitize-with", "tests/sanitize_defaults.cfg", "--ignore", "tests/ipynb-test-samples"],
         ],
-        'verbosity': 2,
     }
 
 def task_install_test_deps():
     test_deps = ['matplotlib', 'sympy', 'pytest-cov']
     return {
         'actions': [['pip', 'install'] + test_deps],
-        'verbosity': 2,
     }
 
 def task_build_dists():
